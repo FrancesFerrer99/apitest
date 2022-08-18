43530fcc8be69422c8502260a1d53cb2162d7d9c
Frances <frances.ferrer1999@gmail.com>
2022-08-18 18:21:46 +0200

    Added Card component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react"
+import Card from "./Components/Card.js"
 
 function App() {
+  const [data, setData] = useState([])
+  const [page, setPage] = useState(1)
+
+  useEffect(() =>{
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&image_type=photo&page=${page}`)
+      .then(res => res.json())
+      .then(data => setData(data.hits))
+  }, [page])
+
+  const cards = data.map((item) => {
+    return <Card 
+            source={item.largeImageURL}
+            key={item.id}
+            />
+  })
+
+  function newPage(event){
+    event.preventDefault()
+    setPage(prevPage =>{
+      return prevPage+1
+    })
+    console.log(page)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {cards}
+      <button onClick={newPage}>Load More</button>
     </div>
   );
 }
