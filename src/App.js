1b7584ba66c2a215ff7e0876ac2ed85874eaa2c2
Frances <frances.ferrer1999@gmail.com>
2022-08-19 20:36:39 +0200

    added search bar, button to load more, styling

--- src/App.js
+++ src/App.js
@@ -3,34 +3,50 @@ import Card from "./Components/Card.js"
 
 function App() {
   const [data, setData] = useState([])
-  const [page, setPage] = useState(1)
+  const [page, setPage] = useState(20)
+  const [search, setSearch] = useState('')
 
   useEffect(() =>{
-    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&image_type=photo&page=${page}`)
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&image_type=photo&per_page=${page}&safesearch=true&q=${search}`)
       .then(res => res.json())
       .then(data => setData(data.hits))
-  }, [page])
+  }, [page, search])
 
   const cards = data.map((item) => {
-    return <Card 
-            source={item.largeImageURL}
+    return <Card
             key={item.id}
+            source={item.webformatURL}
+            author={item.user}
+            views={item.views}
+            downloads={item.downloads}
             />
   })
 
-  function newPage(event){
-    event.preventDefault()
+  function newPage(){
     setPage(prevPage =>{
-      return prevPage+1
+      return prevPage+12
     })
-    console.log(page)
+  }
+
+  function onType(event){
+    const {name, value} = event.target;
+    setSearch(()=> value)
   }
 
   return (
-    <div className="container">
-      {cards}
-      <button onClick={newPage}>Load More</button>
-    </div>
+    <main>
+      <div className="form-container">
+        <form className="form">
+            <input className="form-input" onChange={onType} name="search" value={search} placeholder="search..."/>
+        </form>
+      </div>
+      <div className="container">
+        {cards}
+      </div>
+      <div className="search-button-container">
+        <button onClick={newPage} className="search-button">Load More Images</button>
+      </div>
+    </main>
   );
 }
 
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,8 +2,11 @@ import React from "react"
 
 function Card(props){
     return(
-        <div>
-            <img src={props.source}/>
+        <div className="container-image">
+            <img src={props.source} alt=""/>
+            <h1 className="image-author">{props.author}</h1>
+            <h3 className="image-text"><strong>Views:</strong> {props.views}</h3>
+            <h3 className="image-text"><strong>Downloads:</strong> {props.downloads}</h3>
         </div>
     )
 }
